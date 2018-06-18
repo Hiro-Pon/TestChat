385b46dcf62beaf37c4e63868232f73c8ab6e8ef
Hiroya NAKAJIMA <hiroya.nakajima@gmail.com>
2018-06-18 13:58:21 +0900

    はじめ

---
+++ TestChat/SignupViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SignupViewController.swift
+//  TestChat
+//
+//  Created by 中嶋裕也 on 2018/06/18.
+//  Copyright © 2018年 中嶋裕也. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class SignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
